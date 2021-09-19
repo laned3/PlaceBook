b11683a5812321a25334ca48de9bb36f602896ce
laned3 <89052920+laned3@users.noreply.github.com>
2021-09-19 13:32:15 -0400

    Commit

---
+++ app/src/main/java/com/lanedever/placebook/util/FileUtils.kt
@@ -0,0 +1,12 @@
+package com.lanedever.placebook.util
+
+import android.content.Context
+import java.io.File
+
+object FileUtils {
+    fun deleteFile(context: Context, filename: String) {
+        val dir = context.filesDir
+        val file = File(dir, filename)
+        file.delete()
+    }
+}
